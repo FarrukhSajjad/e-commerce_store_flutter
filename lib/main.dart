19e1f4a5d4f81c852db93f2b484adada66f447d0
Farrukh <farrukhsajjad1998@gmail.com>
2021-03-09 17:04:23 +0500

    2nd

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,7 @@ import 'package:shopify/home.dart';
 import 'package:shopify/screens/products_view.dart';
 
 void main() {
-  // ShopifyConfig.setConfig(
-  //   "4111c8422f78922a732caa88f4f25397",
-  //   "priceinpakistanpk.com",
-  //   "2021-01",
-  // );
+
 
   runApp(MyApp());
 }
